2d302067caf5d8e62fe45179f02d9b20cd49573d
FitsumMehari <m.fitsummehari@gmail.com>
2024-11-17 10:11:52 +0300

    add routes

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const hobbyRoute = require('./routes/hobby');
+const dreamRoute = require('./routes/dream');
+const nameRoute = require('./routes/name');
 
 const app = express();
 
 const PORT = 3000;
 
 app.use(express.json());
+
+app.use("/name", nameRoute);
+app.use("/hobby", hobbyRoute);
+app.use("/dream", dreamRoute);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
