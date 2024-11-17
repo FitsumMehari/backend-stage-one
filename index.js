eb88f78814169504f1c96ddddcb8b53995c26e10
FitsumMehari <m.fitsummehari@gmail.com>
2024-11-17 10:53:41 +0300

    add a handler for unknown routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hobbyRoute = require('./routes/hobby');
 const dreamRoute = require('./routes/dream');
 const nameRoute = require('./routes/name');
+const { redirect } = require("express/lib/response");
 
 const app = express();
 
@@ -12,6 +13,10 @@ app.use(express.json());
 app.use("/name", nameRoute);
 app.use("/hobby", hobbyRoute);
 app.use("/dream", dreamRoute);
+
+app.use((req, res) => {
+    res.redirect("/name")
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port: ${PORT}`);
